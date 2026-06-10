c88fafcebf3866f5c04591df98966b384d6ee5f4
Hans Diaz <hansdiaz2356@gmail.com>
2026-06-10 10:53:46 +0800

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+function App() {
+  return (
+    <h1 className="text-3xl font-serif font-bold underline text-red-500">
+      My Portfolio
+    </h1>
+  )
+}
+
+export default App
